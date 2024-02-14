8551cf00292fefa21aed3acd32ab53d2a7cab3f1
i-xi-dev <naoyuki.isono@gmail.com>
2024-02-14 22:51:13 +0900

    2.0.3

--- deps.ts
+++ deps.ts
@@ -2,11 +2,11 @@ export {
   CodePoint,
   Rune,
   StringEx,
-} from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.4/mod.ts";
 export {
   Radix,
   Uint32,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
-export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.0/mod.ts";
-export { TextEncoding } from "https://raw.githubusercontent.com/i-xi-dev/text-encoding.es/6.3.5/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.3.0/mod.ts";
+export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.1/mod.ts";
+export { TextEncoding } from "https://raw.githubusercontent.com/i-xi-dev/text-encoding.es/6.3.6/mod.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
 export {
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.211.0/assert/mod.ts";
+} from "https://deno.land/std@0.215.0/assert/mod.ts";
